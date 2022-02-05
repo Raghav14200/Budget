7b0a83b60daa27b9a5dc60432e1fefc7c82126c1
Raghavendra Achar C <61545949+Raghav14200@users.noreply.github.com>
2022-02-05 22:14:31 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$signup=mysqli_connect("sql6.freemysqlhosting.net","sql6470608","fk8386njgv","budget") or die(mysqli_error($signup));
+$signup=mysqli_connect("sql6.freemysqlhosting.net","sql6470608","fk8386njgv","sql6470608") or die(mysqli_error($signup));
 
 
 
