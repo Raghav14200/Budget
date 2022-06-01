eb39a8d0cda4097dba772bfc3c007f3c86cfa1b6
Raghavendra Achar C <61545949+Raghav14200@users.noreply.github.com>
2022-06-01 17:17:35 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,5 @@
 <?php
 $con=mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, NULL, NULL, NULL);
 $signup=mysqli_connect("budgetserver.mysql.database.azure.com","budget@budgetserver","password1#","budget") or die(mysqli_error($signup));
 
 
